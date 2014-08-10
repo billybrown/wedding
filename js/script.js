951cdae30775230b2f5d78420d34544a1e506fa4
Bill Brown <bill@echoditto.com>
2014-08-10 11:36:31 -0500

    Updated text

--- js/script.js
+++ js/script.js
@@ -3,26 +3,25 @@
 	
 	$(document).ready(function() {
 
-	});
-
 
 
 	$('#wedding-parking').click(function(){
 		$(this).parent().addClass("is-open");
 	});
+
 	$('#rec-parking').click(function(){
 		$(this).parent().addClass("is-open");
 	});
 
-	$('.cover').click(function(){
+	$('.cover').click(function(){ s
 		$(this).parent().removeClass("is-open");
 	});
 
+	$('.section--sigil').waypoint(function(direction) {
+		$(this).toggleClass("is-out");
+	});
 
-		$('.section--sigil').waypoint(function(direction) {
-			$(this).toggleClass("is-out");
-		});
-
+	});
 
 
 })(jQuery);
